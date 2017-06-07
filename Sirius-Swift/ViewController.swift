61112e4d5846a0a3dcd462421f7ba878a3949fdd
Just <xiaofei0127@outlook.com>
2017-06-07 10:13:03 +0800

    Initial Commit

---
+++ Sirius-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sirius-Swift
+//
+//  Created by PF on 2017/6/7.
+//  Copyright © 2017年 PF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
